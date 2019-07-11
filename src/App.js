2119874280dcafbc9af64227fc210250fa672a1f
Heather <43322948+Heather-Fin@users.noreply.github.com>
2019-07-11 11:02:41 -0500

    Create Radar component

--- src/App.js
+++ src/App.js
@@ -1,77 +1,14 @@
-import React, { Component } from "react";
-import Chart from "react-apexcharts";
+import React from "react";
 import './App.css';
+import Radar from './components/Radar';
 
-class App extends Component {
+function App() {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      options: {
-        chart: {
-          id: "radar",
-          //hides ability to download chart
-          toolbar: {
-            show: false
-          }
-        },
-        plotOptions: {
-          radar: {
-            polygons: {
-              //Graph colors
-              strokeColors: '#e8e8e8',
-              connectorColors: '#e8e8e8',
-              fill: {
-                colors: undefined
-              }
-            }
-          }
-        },
-        colors: ['#a66efa', '#ee538b', '#6ea6ff'],
-        markers: {
-          size: 0
-        },
-        stroke: {
-          width: 2
-        },
-        fill: {
-          opacity: 0.5
-        },
-        xaxis: {
-          categories: ['Gender', 'Race', 'Accessibility', 'Education', 'Economic Background', 'Country of Origin', 'Sexual Identity']
-        },
-        yaxis: {
-          show: false
-        }
-      },
-      series: [
-        {
-          name: "IBM Average",
-          data: [30, 40, 45, 50, 20, 100, 70],
-        },
-        {
-          name: "Your Team",
-          data: [40, 80, 90, 35, 80, 20, 20],
-        }
-      ]
-    };
-  }
-  render() {
-    return (
-      <div className="app">
-        <div className="row">
-          <div className="mixed-chart">
-            <Chart
-              options={this.state.options}
-              series={this.state.series}
-              type="radar"
-              width="600"
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Radar></Radar>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Radar.js
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import Chart from "react-apexcharts";
+
+class Radar extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+        options: {
+          chart: {
+            id: "radar",
+            //hides ability to download chart
+            toolbar: {
+              show: false
+            }
+          },
+          plotOptions: {
+            radar: {
+              polygons: {
+                //Graph colors
+                strokeColors: '#e8e8e8',
+                connectorColors: '#e8e8e8',
+                fill: {
+                  colors: undefined
+                }
+              }
+            }
+          },
+          colors: ['#a66efa', '#ee538b', '#6ea6ff'],
+          markers: {
+            size: 0
+          },
+          stroke: {
+            width: 2
+          },
+          fill: {
+            opacity: 0.5
+          },
+          xaxis: {
+            categories: ['Gender', 'Race', 'Accessibility', 'Education', 'Economic Background', 'Country of Origin', 'Sexual Identity']
+          },
+          yaxis: {
+            show: false
+          }
+        },
+        series: [
+          {
+            name: "IBM Average",
+            data: [30, 40, 45, 50, 20, 100, 70],
+          },
+          {
+            name: "Your Team",
+            data: [40, 80, 90, 35, 80, 20, 20],
+          }
+        ]
+      };
+    }
+    render() {
+      return (
+        <div className="app">
+          <div className="row">
+            <div className="mixed-chart">
+              <Chart
+                options={this.state.options}
+                series={this.state.series}
+                type="radar"
+                width="600"
+              />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  }
+
+export default Radar;
